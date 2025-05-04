67693c00e3f0a0eb0deed07225d4ea0eb2d31fe9
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2025-05-05 00:44:11 +0300

    first acceptable n^2 perfect hash (worked up to 336 words with distinct string hashCode() methods , used pre-known corpus size and dynamic resizing even with n = 1 and still worked , need to find more words with distinct hashCodes

--- demo/src/main/java/perfecthashing/Hashes/QuadraticHash.java
+++ demo/src/main/java/perfecthashing/Hashes/QuadraticHash.java
@@ -1,5 +1,7 @@
 package perfecthashing.Hashes;
 
+import static org.junit.Assert.fail;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -11,169 +13,169 @@ public class QuadraticHash {
     private String[] keys;
     private int size = 0;        // tracks number of inserted keys
     private int n;
+    private int rehashes;
+    private boolean knownCorpus;
     public QuadraticHash() {
-        this.n = 1000;           // initial estimate
+        this.n = 1;           // initial estimate
+        this.rehashes = 0;
+        this.size = 0;
         this.m = n * n;
-        this.p = getNextPrime(100_003);
-        rehashWithNewFunction(new String[0]);  // empty table initially
+        this.p = getPrime(15_000_000);
+        a = rand.nextInt(p-1) + 1;
+        b = rand.nextInt(p);
+        table = new String[m];
+        knownCorpus = false;
     }
     public QuadraticHash(String[] keys) {
         this.keys = keys;
-        int n = keys.length;
-        this.m = n * n;
-        this.p = getNextPrime(100_003); // A prime larger than any reasonable hashCode()
-
+        knownCorpus = true;
+        this.n = keys.length;
+        this.m = n*n;
+        this.size = 0;
+        this.rehashes = 0;
+        this.p = getPrime(15_000_000);
+        rehash(keys);
+    }
+    public int getRehashes()
+    {
+        return rehashes;
+    }
+    public int getSize()
+    {
+        return size;
+    }
+    private void rehash(String [] keys)
+    {
         while (true) {
-            System.out.println("here");
-            a = rand.nextInt(p - 1) + 1; // a ∈ [1, p-1]
-            b = rand.nextInt(p); // b ∈ [0, p)
-            table = new String[m];
-
-            if (tryHashing(this.keys)) {
+            System.out.println("n is currently : " + n);
+            System.out.println("m is currently : " + m);
+            System.out.println("a is currently : " + a);
+            System.out.println("b is currently : " + b);
+            System.out.println("p is currently : " + p);
+            boolean exit = false;
+            for (int i = 0 ; i < 10 ; i ++ )
+            {
+                a = rand.nextInt(p-1) + 1;
+                b = rand.nextInt(p);
+                table = new String[m];
+                if (tryHashing(keys))
+                {
+                    exit = true;
+                    break;
+                }
+            }
+            if(exit)
+            {
                 break;
+            }
+            n++;
+            m = n*n;
+            if(m>1e7)
+            {
+                System.out.println("Maximum Table Size reached");
+                System.exit(-1);
             }
         }
     }
-
-    private boolean tryHashing(String[] keys) {
+    private boolean tryHashing(String[] keys)
+    {
         Arrays.fill(table, null);
-        for (String key : keys) {
+        for (String key : keys)
+        {
             int index = hash(key);
-            if (table[index] != null) {
-                return false; // collision occurred
+            if (table[index] != null)
+            {
+                return false;
             }
             table[index] = key;
             size++;
         }
-        size = 0;
         Arrays.fill(table, null);
+        size = 0;
         return true;
     }
-
-    public boolean insert(String word) {
-        if (contains(word)) {
-            System.out.println("Item already inserted in Hash Table");
-            return false;
-        }
-    
-        if (size + 1 > n) { // Resize and rehash when size exceeds threshold
-            resize();
-        }
-    
-        int index = hash(word);
-        while (table[index] != null) {
-            index = (index + 1) % m; // simple linear probing fallback (or retry hash if strict)
-        }
-    
-        table[index] = word;
-        size++;
-        System.out.println("Successfully inserted");
-        return true;
-    }
-    private void resize() {
-        n *= 2;  // Double the value of n
-        m = n * n;  // m = n^2
-    
-        if (m <= 1) {
-            m = 2;  // Ensure that m is at least 2
-        }
-    
-        p = getNextPrime(m);  // Get the next prime greater than or equal to m
-    
-        // Rehash existing keys with the new table size
-        String[] existingKeys = Arrays.stream(table)
-                                      .filter(Objects::nonNull)
-                                      .toArray(String[]::new);
-        rehashWithNewFunction(existingKeys);
-    }
-    private void rehashWithNewFunction(String[] keys) {
-        while (true) {
-            a = rand.nextInt(p - 1) + 1; // a ∈ [1, p-1]
-            b = rand.nextInt(p); // b ∈ [0, p)
-            table = new String[m];  // Reset table size to new m value
-    
-            if (tryHashing(keys)) {
-                size = keys.length; // Set the size to the number of existing keys
-                break;
-            }
-        }
-    }
-    public boolean delete(String word) {
-        int index = hash(word);
-        if (table[index] == null || !table[index].equals(word)) {
-            System.out.println("Item not present in Hash Table");
-            return false;
-        }
-        table[index] = null;
-        size--;
-        System.out.println("Successfully deleted");
-        return true;
-    }
-    public boolean search(String word) {
-        int index = hash(word);
-        if (table[index] == null || !table[index].equals(word)) {
-            System.out.println("Item not present in Hash Table");
-            return false;
-        }
-        System.out.println("Item Found");
-        return true;
-    }
-
-    public void batchInsert(String path) throws IOException {
-        Scanner scanner = new Scanner(new File(path));
-        while (scanner.hasNextLine()) {
-            String word = scanner.nextLine().trim();
-            insert(word);
-        }
-        scanner.close();
-
-    }
-
-    public void batchDelete(String path) throws IOException {
-        Scanner scanner = new Scanner(new File(path));
-        while (scanner.hasNextLine()) {
-            String word = scanner.nextLine().trim();
-            delete(word);
-        }
-        scanner.close();
-    }
-
-    private int hash(String key) {
-        if (p == 0) {
-            throw new IllegalStateException("Prime number 'p' is zero!");
-        }
-    
-        // Debugging statements to verify values of m and p
-        System.out.println("Hashing key: " + key);
-        System.out.println("Prime p: " + p);
-        System.out.println("Table size m: " + m);
-        System.out.println("Size is  : "+size);
-        long x = key.hashCode() & 0xFFFFFFFFL; // Treat as unsigned
-        long hash = ((a * x + b) % p) % m;
-        return (int) hash;
-    }
-
-    public boolean contains(String key) {
+    public boolean insert(String key) {
         int index = hash(key);
-        return table[index] != null && table[index].equals(key);
-    }
-
-    private int getNextPrime(int min) {
-        while (true) {
-            if (isPrime(min)) {
-                return min;
+        if (size + 1 > n) {
+            resize();
+            rehashes++;
+            index = hash(key); // Recalculate index after resize
+        }
+    
+        if (table[index] != null) {
+            if (!knownCorpus) {
+                String[] existing_keys = Arrays.stream(table).filter(Objects::nonNull).toArray(String[]::new);
+                rehash(existing_keys);
+                for (String word : existing_keys) {
+                    int idx = hash(word);
+                    table[idx] = word;
+                    size++;
+                }
+                index = hash(key); // Recalculate index after rehash
+            } else {
+                return false;
             }
+        }
+    
+        table[index] = key;
+        size++;
+        System.out.println("Item inserted");
+        return true;
+    }
+    public boolean delete(String key)
+    {
+        int index = hash(key);
+        if (table[index]==null || !table[index].equals(key))
+        {
+            System.out.println("Item not in Hashmap");
+            return false;
+        }
+        size--;
+        table[index] = null;
+        System.out.println("Item Deleted");
+        return true;
+    }
+    public void display()
+    {
+        String[] existing_keys = Arrays.stream(table).filter(Objects::nonNull).toArray(String[]::new);
+        for (String key : existing_keys)
+        {
+            System.out.print(key+ " ");
+        }
+        System.out.println();
+    }
+    private void resize()
+    {
+        n = 2*n;
+        m = n*n;
+        String[] existingKeys = Arrays.stream(table)
+                                  .filter(Objects::nonNull)
+                                  .toArray(String[]::new);
+        table = new String[m];
+        rehash(existingKeys);
+        for(String key : existingKeys)
+        {
+            this.insert(key);
+        }
+    }
+    private int getPrime(int min)
+    {
+        while (true) {
+            if (isPrime(min)) return min;
             min++;
         }
     }
-
-    private boolean isPrime(int num) {
-        if (num < 2)
-            return false;
+    private boolean isPrime(int num)
+    {
+        if (num < 2) return false;
         for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0)
-                return false;
+            if (num % i == 0) return false;
         }
         return true;
+    }
+    private int hash(String key) {
+        long x = Math.abs((long) key.hashCode());
+        long hash = ((a * x + b) % p) % m;
+        return (int) hash;
     }
 }
--- demo/src/main/java/perfecthashing/QuadraticHashDemo.java
+++ demo/src/main/java/perfecthashing/QuadraticHashDemo.java
@@ -1,14 +1,99 @@
 package perfecthashing;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import perfecthashing.Hashes.QuadraticHash;
 
 public class QuadraticHashDemo {
     public static void main(String[] args) {
         String[] corpus = {
-            "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"
+    "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
+    "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+    "this", "but", "his", "by", "from", "they", "we", "say", "her", "she",
+    "or", "an", "will", "my", "one", "all", "would", "there", "their", "what",
+    "so", "up", "out", "if", "about", "who", "get", "which", "go", "me",
+    "when", "make", "can", "like", "time", "no", "just", "him", "know", "take",
+    "people", "into", "year", "your", "good", "some", "could", "them", "see", "other",
+    "than", "then", "now", "look", "only", "come", "its", "over", "think", "also",
+    "back", "after", "use", "two", "how", "our", "work", "first", "well", "way",
+    "even", "new", "want", "because", "any", "these", "give", "day", "most", "us",
+    "is", "are", "was", "were", "been", "being", "has", "had", "does", "did",
+    "having", "doing", "shall", "should", "may", "might", "must", "can", "could", "would",
+    "am", "become", "became", "seem", "seemed", "appear", "appeared", "feel", "felt", "sound",
+    "sounded", "look", "looked", "smell", "smelled", "taste", "tasted", "remain", "remained", "stay",
+    "stayed", "keep", "kept", "turn", "turned", "grow", "grew", "get", "got", "go",
+    "went", "come", "came", "leave", "left", "arrive", "arrived", "return", "returned", "move",
+    "moved", "run", "ran", "walk", "walked", "sit", "sat", "stand", "stood", "lie",
+    "lay", "rise", "rose", "fall", "fell", "jump", "jumped", "climb", "climbed", "crawl",
+    "crawled", "swim", "swam", "fly", "flew", "drive", "drove", "ride", "rode", "sail",
+    "sailed", "float", "floated", "sink", "sank", "flow", "flowed", "drift", "drifted", "slide",
+    "slid", "roll", "rolled", "spin", "spun", "turn", "turned", "twist", "twisted", "bend",
+    "bent", "stretch", "stretched", "shrink", "shrank", "grow", "grew", "expand", "expanded", "contract",
+    "contracted", "open", "opened", "close", "closed", "shut", "shut", "lock", "locked", "unlock",
+    "unlocked", "start", "started", "begin", "began", "stop", "stopped", "end", "ended", "finish",
+    "finished", "continue", "continued", "pause", "paused", "wait", "waited", "stay", "stayed", "remain",
+    "remained", "live", "lived", "die", "died", "exist", "existed", "occur", "occurred", "happen",
+    "happened", "take", "took", "get", "got", "give", "gave", "send", "sent", "receive",
+    "received", "bring", "brought", "carry", "carried", "hold", "held", "keep", "kept", "put",
+    "put", "place", "placed", "set", "set", "lay", "laid", "drop", "dropped", "throw",
+    "threw", "catch", "caught", "pick", "picked", "choose", "chose", "select", "selected", "find",
+    "found", "lose", "lost", "search", "searched", "look", "looked", "see", "saw", "watch",
+    "watched", "observe", "observed", "notice", "noticed", "hear", "heard", "listen", "listened", "sound",
+    "sounded", "speak", "spoke", "talk", "talked", "say", "said", "tell", "told", "ask",
+    "asked", "answer", "answered", "reply", "replied", "respond", "responded", "shout", "shouted", "yell",
+    "yelled", "scream", "screamed", "whisper", "whispered", "call", "called", "name", "named", "mention",
+    "mentioned", "discuss", "discussed", "explain", "explained", "describe", "described", "report", "reported", "announce",
+    "announced", "state", "stated", "declare", "declared", "proclaim", "proclaimed", "say", "said", "speak",
+    "spoke", "talk", "talked", "tell", "told", "ask", "asked", "answer", "answered", "reply",
+    "replied", "respond", "responded", "shout", "shouted", "yell", "yelled", "scream", "screamed", "whisper",
+    "whispered", "call", "called", "name", "named", "mention", "mentioned", "discuss", "discussed", "explain",
+    "explained", "describe", "described", "report", "reported", "announce", "announced", "state", "stated", "declare",
+    "declared", "proclaim", "proclaimed", "say", "said", "speak", "spoke", "talk", "talked", "tell",
+    "told", "ask", "asked", "answer", "answered", "reply", "replied", "respond", "responded", "shout",
+    "shouted", "yell", "yelled", "scream", "screamed", "whisper", "whispered", "call", "called", "name",
+    "named", "mention", "mentioned", "discuss", "discussed", "explain", "explained", "describe", "described", "report",
+    "reported", "announce", "announced", "state", "stated", "declare", "declared", "proclaim", "proclaimed", "say",
+    "said", "speak", "spoke", "talk", "talked", "tell", "told", "ask", "asked", "answer",
+    "answered", "reply", "replied", "respond", "responded", "shout", "shouted", "yell", "yelled", "scream",
+    "screamed", "whisper", "whispered", "call", "called", "name", "named", "mention", "mentioned", "discuss",
+    "discussed", "explain", "explained", "describe", "described", "report", "reported", "announce", "announced", "state",
+    "stated", "declare", "declared", "proclaim", "proclaimed", "say", "said", "speak", "spoke", "talk",
+    "talked", "tell", "told", "ask", "asked", "answer", "answered", "reply", "replied", "respond",
+    "responded", "shout", "shouted", "yell", "yelled", "scream", "screamed", "whisper", "whispered", "call",
+    "called", "name", "named", "mention", "mentioned", "discuss", "discussed", "explain", "explained", "describe",
+    "described", "report", "reported", "announce", "announced", "state", "stated", "declare", "declared", "proclaim",
+    "proclaimed", "say", "said", "speak", "spoke", "talk", "talked", "tell", "told", "ask",
+    "asked", "answer", "answered", "reply", "replied", "respond", "responded", "shout", "shouted", "yell"
         };
-        QuadraticHash hashTable = new QuadraticHash(corpus);
-        hashTable.insert("honeydew");
-        hashTable.insert("apple");
+        boolean similar = false;
+        corpus = Arrays.stream(corpus).distinct().toArray(String[]::new);
+        HashSet<Integer> set = new HashSet<>();
+        for (String word : corpus)
+        {
+            Integer hash = word.hashCode();
+            if (set.contains(hash))
+            {
+                similar = true;
+                break;
+            }
+            set.add(hash);
+        }
+        System.out.println(similar);
+        QuadraticHash hashTable = new QuadraticHash();
+        for (String word : corpus)
+        {
+            hashTable.insert(word);
+        }
+        hashTable.display();
+        System.out.println(hashTable.getSize());
+        System.err.println(hashTable.getRehashes());
+        for (int i = 0 ; i < corpus.length ; i+=2)
+        {
+            hashTable.delete(corpus[i]);
+        }
+        hashTable.display();
+        System.out.println(hashTable.getSize());
+
     }
 }
